74bbeabfbe0a3a56056803895fdd28f73b59f873
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-09 14:50:41 +0300

    initial commit

---
+++ backend/src/app.js
@@ -0,0 +1,27 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./lib/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+const app = express();
+app.use(express.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/auth", authRoutes);
+app.use("/api/product", productRoutes);
+
+app.use((err, req, res, next) => {
+  if (err.name === "ValidationError") {
+    const messages = Object.values(err.errors).map((e) => e.message);
+    return res.status(400).json({ message: messages.join(", ") });
+  }
+
+  console.error(err);
+  res.status(500).json({ message: "Something went wrong" });
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port " + process.env.PORT);
+  connectDB();
+});
