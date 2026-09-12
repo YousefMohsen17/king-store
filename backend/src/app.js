35acf2fca3556908b05762dcb29b4083d0ab6833
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-12 09:13:51 +0300

    add payment feature

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
@@ -15,6 +16,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/product", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupon", couponRoutes);
+app.use("/api/payment", paymentRoutes);
 
 app.use((err, req, res, next) => {
   if (err.name === "ValidationError") {
