97410d287d761e20b70ddf3834ee71646e80f534
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-13 14:20:37 +0300

    remove next function

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -43,14 +43,13 @@ const userSchema = new mongoose.Schema(
   },
 );
 
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) return next();
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) return;
   try {
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
-    next();
   } catch (error) {
-    next(error);
+    console.log("error in userSchema.pre save", error);
   }
 });
 userSchema.methods.comparePassword = async function (password) {
