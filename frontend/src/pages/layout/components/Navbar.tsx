7138d8e6000d44d3374a9a11367bd3c1b00c4239
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-13 14:22:23 +0300

    update navbar in layout

--- frontend/src/pages/layout/components/Navbar.tsx
+++ frontend/src/pages/layout/components/Navbar.tsx
@@ -1,9 +1,12 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
-
+// import { useCartStore } from "../stores/useCartStore";
+import { useAuth } from "../../../context/auth/authContextObject";
 const Navbar = () => {
-  const isAdmin = true;
-
+  const { user, logout } = useAuth();
+  const isAdmin = user?.type === "admin";
+  // const { cart } = useCartStore();
+  const cart = [1, 1];
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
