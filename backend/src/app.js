5a58a919fe75ee6a726ab9614a5610f18320da82
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-13 14:20:22 +0300

    use cors in app and add my site

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,14 @@ import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
+import cors from "cors";
 const app = express();
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
