85c462420012c65f0f96f7cbbd9e3377206fffee
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-15 12:47:49 +0300

    update cart and authenticated in navbar

--- frontend/src/pages/layout/components/Navbar.tsx
+++ frontend/src/pages/layout/components/Navbar.tsx
@@ -1,12 +1,11 @@
 import { ShoppingCart, UserPlus, LogIn, LogOut, Lock } from "lucide-react";
 import { Link } from "react-router-dom";
-// import { useCartStore } from "../stores/useCartStore";
 import { useAuth } from "../../../context/auth/authContextObject";
+import { useCart } from "../../../context/cart/cartContextObject";
 const Navbar = () => {
-  const { user, logout } = useAuth();
+  const { user, logout, authenticated } = useAuth();
   const isAdmin = user?.type === "admin";
-  // const { cart } = useCartStore();
-  const cart = [1, 1];
+  const { cartProducts } = useCart();
   return (
     <header className="fixed top-0 left-0 w-full bg-gray-900 bg-opacity-90 backdrop-blur-md shadow-lg z-40 transition-all duration-300 border-b border-emerald-800">
       <div className="container mx-auto px-4 py-3">
@@ -37,12 +36,12 @@ const Navbar = () => {
                   size={20}
                 />
                 <span className="hidden sm:inline">Cart</span>
-                {cart.length > 0 && (
+                {cartProducts?.length > 0 && (
                   <span
                     className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out"
                   >
-                    {cart.length}
+                    {cartProducts?.length}
                   </span>
                 )}
               </Link>
@@ -58,7 +57,7 @@ const Navbar = () => {
               </Link>
             )}
 
-            {user ? (
+            {authenticated ? (
               <button
                 className="bg-gray-700 hover:bg-gray-600 text-white py-2 px-4 
 						rounded-md flex items-center transition duration-300 ease-in-out"
