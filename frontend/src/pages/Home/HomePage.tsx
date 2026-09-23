5e6aaa503e80e2c23db46873c0b032eb6079ed4c
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-23 13:33:26 +0300

    handle loading in home page

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -2,10 +2,12 @@ import CategoryItem from "./components/CategoryItem";
 import { useProduct } from "../../context/product/productContextObject";
 import FeaturedProducts from "./components/FeaturedProducts.tsx";
 export default function HomePage() {
-  const { categories, featuredProducts, featuredProductsLoading } =
-    useProduct();
-  //   const { fetchFeaturedProducts, products, isLoading } = useProductStore();
-
+  const {
+    categories,
+    featuredProducts,
+    featuredProductsLoading,
+    categoriesLoading,
+  } = useProduct();
   return (
     <div className="relative min-h-screen text-white overflow-hidden">
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
@@ -15,12 +17,15 @@ export default function HomePage() {
         <p className="text-center text-xl text-gray-300 mb-12">
           Discover the latest trends in eco-friendly fashion
         </p>
-
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {categories?.map((category) => (
-            <CategoryItem category={category} key={category} />
-          ))}
-        </div>
+        {categoriesLoading ? (
+          <div className="flex justify-center">Loading...</div>
+        ) : (
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            {categories?.map((category) => (
+              <CategoryItem category={category} key={category} />
+            ))}
+          </div>
+        )}
 
         {!featuredProductsLoading && featuredProducts?.length > 0 && (
           <FeaturedProducts featuredProducts={featuredProducts} />
