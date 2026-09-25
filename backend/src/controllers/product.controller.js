a923f526ce2f569db2a5b4db01958985dc0352fa
Yousef Mohsen <ymohsen746@gmail.com>
2026-09-25 15:32:17 +0300

    add get recommended products api

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -37,6 +37,30 @@ export async function deleteProduct(req, res) {
     return res.status(500).json({ message: "Internal server error" });
   }
 }
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 4 },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ]);
+
+    res.json(products);
+  } catch (error) {
+    console.log("Error in getRecommendedProducts controller", error.message);
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
+
 export async function createProduct(req, res) {
   try {
     const { name, description, price, productImg, category } = req.body;
@@ -111,7 +135,7 @@ export async function addFeaturedProducts(req, res) {
     return res.status(500).json({ message: "Internal server error" });
   }
 }
-async function updateFeaturedProductsCache() {
+export async function updateFeaturedProductsCache() {
   try {
     const featuredProducts = await Product.find({ featured: true }).lean();
     await redis.set("featured_products", JSON.stringify(featuredProducts));
